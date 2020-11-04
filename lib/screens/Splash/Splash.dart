d8673b3473f41a84e496e42472a843d0e96b0431
afrasyab591 <afrasyab591@gmail.com>
2020-11-04 20:48:33 +0500

    pull from dev

--- lib/screens/Splash/Splash.dart
+++ lib/screens/Splash/Splash.dart
@@ -4,8 +4,16 @@ import 'package:truckoom_shipper/animations/slide_right.dart';
 import 'package:truckoom_shipper/res/colors.dart';
 import 'package:truckoom_shipper/res/sizes.dart';
 import 'package:truckoom_shipper/routes/routes.dart';
+import 'package:truckoom_shipper/screens/bookLoadDetails/book_load_details.dart';
+import 'package:truckoom_shipper/screens/bottomTab/bottom_tab.dart';
+import 'package:truckoom_shipper/screens/businessEditProfile/business_edit_profile.dart';
+import 'package:truckoom_shipper/screens/businessProfile/business_profile.dart';
 import 'package:truckoom_shipper/screens/checkUserType/check_user.dart';
 import 'package:truckoom_shipper/screens/forgotPassword/forgot_password.dart';
+import 'package:truckoom_shipper/screens/individualEditProfile/individual_edit_profile.dart';
+import 'package:truckoom_shipper/screens/individualProfile/individual_profile.dart';
+import 'package:truckoom_shipper/screens/invoiceDetail/invoice_detail.dart';
+import 'package:truckoom_shipper/screens/jobDetails/job_details.dart';
 import 'package:truckoom_shipper/screens/language/language.dart';
 import 'package:truckoom_shipper/screens/login/login.dart';
 
@@ -20,7 +28,7 @@ class _SplashState extends State<Splash> {
   void onDelay() async{
     await Future.delayed(Duration(seconds: 3), (){
       // Navigator.pushNamed(context, Routes.checkUser);
-      Navigator.pushReplacement(context, SlideRightRoute(page: Login()));
+      Navigator.pushReplacement(context, SlideRightRoute(page: CheckUser()));
     });
   }
 
